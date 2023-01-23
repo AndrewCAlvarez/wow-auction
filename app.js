ab4771ffe7fea874988375cbefc619e291dc5b0c
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2023-01-23 16:02:49 -0500

    Add environment variables. Update to Node v18 for fetch support. Removed script from buttons to be replaced with internal api calls from the app to the Blizzard API.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const wowAPI = require("./public/script");
+import express from "express";
+import dotenv from "dotenv";
+import { getAccessToken } from "./public/script.js";
 
 const app = express();
 const port = 3000;
@@ -10,7 +10,17 @@ console.log(process.env.CLIENT_ID);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  console.log("Getting index.html");
   res.send("./index.html");
+});
+
+app.get("/api", (req, res) => {
+  console.log("Get /api");
+  getAccessToken(
+    "701a8a5976ab4346ba5c4b151ca76132",
+    "MAh3G6I93NogGlz3Kwy8jbdknDJklagd",
+    "client_credentials"
+  );
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+// If there are errors with this module in github pages, check firefox and other sources for changing the file type to .mjs
+
 let accessToken = "";
 const hostName = "us.api.blizzard.com";
 const namespace = "namespace=dynamic-us&locale=en_US";
@@ -13,7 +15,11 @@ const options = {
   method: "POST",
 };
 
-function getAccessToken(clientId, clientSecret, grantType) {
+export function getAccessToken(clientId, clientSecret, grantType) {
+  let accessToken = "";
+  const hostName = "us.api.blizzard.com";
+  const namespace = "namespace=dynamic-us&locale=en_US";
+
   const fetchTokenPromise = fetch(
     `https://oauth.battle.net/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=${grantType}`,
     options
@@ -32,7 +38,7 @@ function getAccessToken(clientId, clientSecret, grantType) {
   console.log("Fetching access token...");
 }
 
-function getConnectedRealms() {
+export function getConnectedRealms() {
   fetch(
     `https://${hostName}/data/wow/connected-realm/index?${namespace}&access_token=${accessToken}`
   )
@@ -43,5 +49,4 @@ function getConnectedRealms() {
       console.log(ConnectedRealms);
     });
 }
-
-module.exports = { getAccessToken, getConnectedRealms };
+// export { getAccessToken, getConnectedRealms };
