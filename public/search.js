52a29ff96f331ac40a6979e89dfccf2cdeca28cc
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2023-02-09 08:44:41 -0500

    Search now creates empty links and pulls up correct results. Delay now where deleted search term pulls up results.

--- public/search.js
+++ public/search.js
@@ -1,50 +1,64 @@
 import { auctionSampleData } from "./auctionSampleData.js";
 import { getItemById, getItemMedia } from "./script.js";
 
+// TODO: Old results are coming in during search. Need to check if new search has come in.
+
+let searchTerm = "";
+
 const auctionSearch = document.querySelector(".auctionSearch");
+
 auctionSearch.addEventListener("keyup", (e) => {
-  let currentValue = e.target.value.toLowerCase();
-  // auctionSampleData.auctions.forEach((auction) => {
-  //   console.log(getItemById(auction.item.id));
-  // });
-  if (currentValue === "") {
-  } else {
-    const url = "http://127.0.0.1:3000/api/search";
-    fetch(url + `?name=${currentValue}`)
-      .then((response) => response.json())
-      .then((searchResults) => {
-        console.log(searchResults.results);
-        clearSearch();
-        searchResults.results.forEach((item) => {
-          populateSearch(item.data.id);
-        });
-      });
-  }
+  searchTerm = e.target.value.toLowerCase().trim();
+
+  clearSearch();
+  executeSearch(searchTerm);
 });
 
+async function executeSearch(searchTerm) {
+  if (searchTerm.length === 0) {
+    // Do nothing
+  } else {
+    let resultObj = await search(searchTerm);
+    resultObj.data.results.forEach((item) => {
+      populateSearch(item.data.id);
+    });
+  }
+}
+
+async function search(searchTerm) {
+  const url = "http://127.0.0.1:3000/api/search" + `?name=${searchTerm}`;
+  let searchObj = {};
+
+  await fetch(url)
+    .then((response) => response.json())
+    .then((searchResults) => {
+      searchObj.searchTerm = searchTerm;
+      searchObj.data = searchResults;
+    });
+
+  return searchObj;
+}
+
 async function populateSearch(itemId) {
-  let item;
-  let itemMedia;
   let searchResultContainer = document.querySelector(".searchResultContainer");
-  await getItemById(itemId).then((item) => {
-    item = item;
-    getItemMedia(itemId)
-      .then((itemMedia) => {
-        itemMedia = itemMedia;
-      })
-      .then(() => {
-        console.log(item.name);
-        let searchResult = document.createElement("p");
-        searchResult.className = "searchResult";
-        searchResult.textContent = item.name;
-        searchResultContainer.appendChild(searchResult);
-      });
-  });
+  let item = await getItemById(itemId);
+
+  if (!document.querySelector(`#item${itemId}`)) {
+    let searchResult = document.createElement("a");
+    let searchResultText = document.createElement("p");
+    searchResult.className = "searchResult";
+    searchResult.id = "item" + itemId;
+    searchResult.setAttribute("href", "#");
+    searchResultText.className = "searchResultText";
+    searchResultText.textContent = item.name;
+
+    searchResult.appendChild(searchResultText);
+    searchResultContainer.appendChild(searchResult);
+  }
 }
 
 function clearSearch() {
   let searchResults = document.querySelectorAll(".searchResult");
-  console.log(searchResults);
   searchResults.forEach((searchResult) => {
     if (searchResult.parentNode) {
       searchResult.parentNode.removeChild(searchResult);
